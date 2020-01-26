be9c606017eda750a782bb08d4b2bf301666466c
liamSully43 <liamsullivan43@gmail.com>
2020-01-26 21:39:01 +0000

    Updates

---
+++ precache-manifest.c3a43131ca3a0a642af399d23924cada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8fdeca17788fb950c0ed4c951efbac",
+    "url": "/wheel-of-fortune/index.html"
+  },
+  {
+    "revision": "0c8087cd5efe127dfe98",
+    "url": "/wheel-of-fortune/static/js/2.9b2939a7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/wheel-of-fortune/static/js/2.9b2939a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4216dbf29758ecf045b",
+    "url": "/wheel-of-fortune/static/js/main.12f508c7.chunk.js"
+  },
+  {
+    "revision": "be91308eccc64d27e3c4",
+    "url": "/wheel-of-fortune/static/js/runtime-main.1373483b.js"
+  },
+  {
+    "revision": "4fb2415cd7efdaf59c92ea3523334478",
+    "url": "/wheel-of-fortune/static/media/wheel.4fb2415c.png"
+  },
+  {
+    "revision": "dee4e65886c87374608e464956b41a46",
+    "url": "/wheel-of-fortune/static/media/wheel_of_fortune_logo.dee4e658.png"
+  }
+]);
